894bade7b08b06daf7e040076a5ce3dc346f16c0
Mine02C4 <mine@mine-studio.com>
2016-08-02 01:07:35 +0900

    アセンブラでちびちび改善

--- prog_norm/norm.asm
+++ prog_norm/norm.asm
@@ -1,11 +1,13 @@
-	add $1,$0,$0
-	addi $2,$0,100
+	addi $1,$0,396
 	add $3,$0,$0
-loop: lw $4,0($1)
-	addi $2,$2,-1
+loop:
+	lw $4,0($1)
+	addi $1,$1,-4
 	mult $4,$4,$4
-	addi $1,$1,4
-	bne $2,$0,loop
+	bne $1,$0,loop
+	add $3,$3,$4
+	lw $4,0($1)
+	mult $4,$4,$4
 	add $3,$3,$4
 	sw  $3, 0x7fff($0)
 end: beq $0,$0,end
